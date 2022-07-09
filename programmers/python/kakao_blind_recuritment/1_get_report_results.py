0093a4bab11ae8916b1c86eaa068f0dc47886ff4
hyeon <selwha1@naver.com>
2022-07-09 21:57:51 +0900

    신고 결과 받기 다른사람풀이 추가

--- programmers/python/kakao_blind_recuritment/1_get_report_results.py
+++ programmers/python/kakao_blind_recuritment/1_get_report_results.py
@@ -30,3 +30,25 @@ def solution(id_list, reports, k):
 
 # solution(["muzi", "frodo", "apeach", "neo"],["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"],2)
 solution(["con", "ryan"],["ryan con", "ryan con", "ryan con", "ryan con"],3)
+
+
+
+
+
+"""
+다른사람 풀이 추가
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)    
+    reports = {x : 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
+    
+"""

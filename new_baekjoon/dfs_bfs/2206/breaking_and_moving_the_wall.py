6a8787c84a11ad8707b6ecc602c0ecfaed896cdc
hyeon <selwha1@naver.com>
2022-08-30 17:22:32 +0900

    벽 부수고 이동하기

---
+++ new_baekjoon/dfs_bfs/2206/breaking_and_moving_the_wall.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+
+input=sys.stdin.readline
+
+n,m=map(int,input().split())
+
+graph=[[*map(int,input().rstrip())] for _ in range(n)]
+
+visited=[[[0]*2 for _ in range(m)] for _ in range(n)]
+visited[0][0][0] = 1
+
+direction=[(-1,0),(1,0),(0,-1),(0,1)]
+
+def bfs(x,y,z):
+    q=deque()
+    q.append((x,y,z))
+    while q:
+        x,y,z=q.popleft()
+        if x==n-1 and y==m-1:
+            return visited[x][y][z]
+        for nx,ny in direction:
+            nx+=x
+            ny+=y
+            if nx<0 or nx>=n or ny<0 or ny>=m:
+                continue
+            if graph[nx][ny]==1 and z==0: # 벽이고 벽부수기 사용안했을때
+                visited[nx][ny][1]=visited[x][y][0]+1
+                q.append((nx,ny,1))
+            elif graph[nx][ny]==0 and visited[nx][ny][z]==0: # 벽이 아니고 방문하지 않았을때
+                visited[nx][ny][z]=visited[x][y][z]+1
+                q.append((nx,ny,z))
+    return -1
+print(bfs(0,0,0))
